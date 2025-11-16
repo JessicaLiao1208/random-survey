043ef915a8ad5d18ebcfacb6b9ff38637226837b
JessicaLiao1208 <wen.20001208@gmail.com>
2025-11-16 23:32:04 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,7 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://www.youtube.com/',
+	'https://www.netflix.com/browse',
+	'https://chatgpt.com/',
 ];
